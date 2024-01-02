429aa922817e5f6a237efd94311529bc8b507d72
unknown <piotr.mikulski@gmail.com>
2024-01-02 00:08:24 -0800

    Fix build

--- build_exe.ps1
+++ build_exe.ps1
@@ -6,10 +6,12 @@ $exeName = "app_muter.exe"
 New-Item -ItemType Directory -Path .\dist
 
 # Install pyinstaller if not already installed
+pip install pycaw
 pip install pyinstaller 
 
 # Convert Python app to EXE in dist folder 
-pyinstaller --onefile --distpath .\dist $pythonApp
+# pyinstaller --onefile --hidden-import=pycaw --distpath .\dist $pythonApp
+pyinstaller app_muter.spec
 
 # Move the EXE to the current folder
 # Move-Item -Path .\dist\$exeName -Destination .\ 
