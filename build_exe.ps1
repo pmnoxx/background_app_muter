f0e2ab4850b99feb6daedc4093998c8516de623e
unknown <piotr.mikulski@gmail.com>
2024-01-02 01:37:53 -0800

    Update build to not print warning

--- build_exe.ps1
+++ build_exe.ps1
@@ -3,7 +3,9 @@ $pythonApp = "app_muter.py"
 $exeName = "app_muter.exe" 
 
 # Create dist folder if it doesn't exist
-New-Item -ItemType Directory -Path .\dist
+if (!(Test-Path -Path .\dist)) {
+  New-Item -ItemType Directory -Path .\dist
+}
 
 # Install pyinstaller if not already installed
 pip install pycaw
